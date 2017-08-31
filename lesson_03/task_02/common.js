c5e798e68f031efaf0ad6a421bc2e468770085ac
Kirigam <hrytsiv.a.s"gmail.com>
2017-08-31 15:56:34 +0300

    Add task_02

---
+++ lesson_03/task_02/common.js
@@ -0,0 +1,13 @@
+var search = document.location.search;
+
+function add(argument) {
+	var argument = argument.split('&');
+	var sum = 0;
+	for(var i = 0; i < argument.length; i++) {
+		var index = argument[i].indexOf('=');
+		sum += +argument[i].substr(++index);
+	}
+	return document.write(sum);
+}
+
+add(search);
